e2eb8ad5c0b3f2ac09e1f59547bbfe83f75045d8
MartynaRogala <martyna1.rogala@doctoral.uj.edu.pl>
2021-05-10 20:28:31 +0200

    Ex. 3 done

--- homework_week8.py
+++ homework_week8.py
@@ -16,4 +16,15 @@ print("array (v1) is:", v1)
 v2 = v1[1::2]
 print("array (v2) is:", v2)
 v3 = v1[::-1]
-print("array (v3) is:", v3)
+print("array (v3) is:", v3, "\n")
+
+print("Exercise 8.3")
+
+m1 = np.arange(0, 20, 1, dtype=int).reshape(4, 5)
+print("array (m1) is:","\n", m1, "\n")
+m2 =np.fliplr(m1)
+print("array (m2) is:", "\n", m2, "\n")
+m3 =np.flipud(m1)
+print("array (m3) is:", "\n", m3, "\n")
+m1cut = m1[1:-1,1:-1]
+print("array (m1) with cutting is:", "\n", m1cut, "\n")
